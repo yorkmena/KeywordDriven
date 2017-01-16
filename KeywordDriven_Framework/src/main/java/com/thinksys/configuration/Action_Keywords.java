27fc753e5296d901517537d4fda7c231201a98c1
asharma1586 <sharma.anamika86@gmail.com>
2017-01-16 21:11:04 +0530

    updated gitignore and action keywords

--- KeywordDriven_Framework/src/main/java/com/thinksys/configuration/Action_Keywords.java
+++ KeywordDriven_Framework/src/main/java/com/thinksys/configuration/Action_Keywords.java
@@ -1,6 +1,12 @@
 package com.thinksys.configuration;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.regex.Pattern;
+
+import org.apache.http.util.Asserts;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,7 +18,7 @@ public class Action_Keywords {
 	
 	public static WebDriver driver;
 	
-	public  void performAction(String ActionKeyword, String locatorType, String locatorValue, String data)
+	public  void performAction(String ActionKeyword, String locatorType, String locatorValue, String data) throws Exception
 	{
 		switch(ActionKeyword)
 		{
@@ -95,7 +101,273 @@ public class Action_Keywords {
 			goback();
 		}
 		break;
+		
+		case "Verifypageproperty":
+		{
+			Verifypageproperty(data);
 		}
+		break;
+		
+		case "selectmultipleitems":
+		{
+			selectmultipleitems(locatorValue, locatorType, data);
+		}
+		break;
+		
+		case "verifytextinpagesource":
+		{
+			verifytextinpagesource(data);
+		}
+		break;
+		
+		case "verifytextnotinpagesource":
+		{
+			verifytextnotinpagesource(data);
+		}
+		break;
+		
+		case "verifytextcontained":
+		{
+			verifytextcontained(locatorValue, locatorType, data);
+		}
+		break;
+		
+		case "verifytextnotcontained":
+		{
+			verifytextnotcontained(locatorValue, locatorType, data);
+		}
+		break;
+		
+		case "verifytextonpage":
+		{
+			verifytextonpage(data);
+		}
+		break;
+		
+		case "verifytextnotonpage":
+		{
+			verifytextnotonpage(data);
+		}
+		break;
+		
+		case "verifylistitempresent":
+		{
+			verifylistitempresent(locatorValue, locatorType, data);
+		}
+		break;
+		
+		case "verifylistitemnotpresent":
+		{
+			verifylistitemnotpresent(locatorValue, locatorType, data);
+		}
+		break;
+		
+		case "verifyobjectdisplayed":
+		{
+			verifyobjectdisplayed(locatorValue, locatorType);
+		}
+		break;
+		
+	/*	case "verifyobjectproperty":
+		{
+			verifyobjectproperty(locatorValue, locatorType, data);
+		}
+		break;*/
+		
+		}
+	} 
+	
+	/*public void verifyobjectproperty(String locatorValue, String locatorType, String data)
+	{
+		By valueOfLocator=locator(locatorType, locatorValue);
+		WebElement w= driver.findElement(valueOfLocator);
+		
+		System.out.println(w.getText());
+	}*/
+	
+	public void verifyobjectdisplayed(String locatorValue, String locatorType) 
+	{
+	
+			try {
+				By valueOfLocator=locator(locatorType, locatorValue);
+				WebElement w= driver.findElement(valueOfLocator);
+				
+				  if(w==null || !w.isDisplayed())
+				  {
+					  System.out.println("Object is not present in the page.");
+				  }
+				  else
+				  {
+					  System.out.println("Object is present.");
+				  }
+				}
+			catch(Exception e)
+			{
+				//If(e.getMessage().)
+				System.out.println("Object is not found.");
+			}
+	}
+	
+	
+	
+	public void verifylistitempresent(String locatorValue, String locatorType, String data)
+	{
+		By valueOfLocator=locator(locatorType, locatorValue);		
+		List<WebElement> w= driver.findElements(valueOfLocator);	
+		
+	//	System.out.println(w.size());
+		boolean a=false;
+		for(int i=0; i<w.size(); i++)
+		{
+			String s= w.get(i).getAttribute("value");
+			if(s.equalsIgnoreCase(data))
+			{
+				System.out.println("Specified list item is present in the list.");
+				a=true;
+			}
+		}
+		if(a==false)
+		{
+			System.out.println("Specified list item is not present in the list.");
+		}
+	}
+	
+	
+	public void verifylistitemnotpresent(String locatorValue, String locatorType, String data)
+	{
+		By valueOfLocator=locator(locatorType, locatorValue);		
+		List<WebElement> w= driver.findElements(valueOfLocator);	
+	//	System.out.println(w.size());
+		boolean a=false;
+		for(int i=0; i<w.size(); i++)
+		{
+			String s= w.get(i).getAttribute("value");
+			if(s.equalsIgnoreCase(data))
+			{
+				System.out.println("Specified list item is present in the list.");
+				a=true;
+			}
+		}
+		if(a==false)
+		{
+			System.out.println("Specified list item is not present in the list.");
+		}
+	}
+	
+	
+	public void verifytextnotonpage(String data)
+	{
+		WebElement w = driver.findElement(By.tagName("body"));
+		boolean e = w.getText().toLowerCase().contains(data.toLowerCase());
+		if(e!=true)
+		{
+			System.out.println("Text is not present on the page.");
+		}
+		else
+		{
+			System.out.println("Text is present on the page.");
+		}
+	}
+	
+	public void verifytextonpage(String data)
+	{
+		WebElement e = driver.findElement(By.tagName("body"));
+		if(e.getText().toLowerCase().contains(data.toLowerCase()))
+		{
+			System.out.println("Text is present on the page.");
+		}
+		else
+		{
+			System.out.println("Text is not present on the page.");
+		}
+	}
+	
+	public void verifytextcontained(String locatorValue, String locatorType, String data)
+	{
+		By valueOfLocator=locator(locatorType, locatorValue);		
+		WebElement w= driver.findElement(valueOfLocator);	
+		WebElement w1=driver.findElement(By.tagName("body"));
+	//	boolean e=w.getText().contains(data);
+	//	System.out.println(w.getText());
+		
+		if(w.getText().toLowerCase().contains(data.toLowerCase()))
+		{
+			System.out.println("Text is present in the specified test object.");
+		}
+		else if(w1.getText().toLowerCase().contains(data.toLowerCase()))
+		{
+			System.out.println("Text is present in the page.");
+		}
+		else 
+		{
+			System.out.println("Text is not present.");
+		}
+	}
+	
+	public void verifytextnotcontained(String locatorValue, String locatorType, String data)
+	{
+		By valueOfLocator=locator(locatorType, locatorValue);		
+		WebElement w= driver.findElement(valueOfLocator);	
+		boolean e=w.getText().toLowerCase().contains(data.toLowerCase());
+		if(e!=true)
+		{
+			System.out.println("Text is not present in the specified test object.");
+		}
+		else
+		{
+			System.out.println("Text is present in the test object.");
+		}
+	}
+	
+	public void verifytextinpagesource(String data)
+	{
+		if(driver.getPageSource().contains(data))
+		{
+			System.out.println("Text is present in page source.");
+		}
+		else
+		{
+			System.out.println("Specified text is not present");
+		}
+	}
+	
+	public void verifytextnotinpagesource(String data)
+	{
+		boolean e=driver.getPageSource().contains(data);
+		if(e!=true)
+		{
+			System.out.println("Specified text is not present in the page source.");
+		}
+		else
+		{
+			System.out.println("Text is present in the page source.");
+		}
+	}
+	
+	public void selectmultipleitems(String locatorValue, String locatorType, String data)
+	{
+		By valueOfLocator=locator(locatorType, locatorValue);		
+		WebElement w= driver.findElement(valueOfLocator);	
+		Select select=new Select(w);
+		
+		String data_array[]= data.split(Pattern.quote("|"));
+		
+		for (String temp: data_array){   
+	          select.selectByVisibleText(temp);
+	       }	
+	}	
+	
+	public void Verifypageproperty(String data)
+	{
+		if(data.equals(driver.getTitle()) || data.equals(driver.getCurrentUrl()))
+		{
+			System.out.println("Specified page property is coming correctly as: " +data);
+		}
+		else
+		{
+			System.out.println("Specified page property is incorrect.");
+		}
+			
 	}
 	
 	public void Launch(String browserName)
@@ -125,36 +397,40 @@ public class Action_Keywords {
 	
 	public void GetPage(String url)
 	{
-		/*System.out.println("In method...Val of URL is:- "+url);
-		System.out.println("In method...Val of locatorVal is:- "+pageName);*/
 		driver.get(url);
 		System.out.println("URL Launched Successfully");
 	}
 	
-	public static By locator(String locatorTpye, String value) {
-		By by;
-		switch (locatorTpye) {
-		case "id":
-			by = By.id(value);
-			break;
-		case "name":
-			by = By.name(value);
-			break;
-		case "xpath":
-			by = By.xpath(value);
-			break;
-		case "css":
-			by = By.cssSelector(value);
-			break;
-		case "linkText":
-			by = By.linkText(value);
-			break;
-		case "partialLinkText":
-			by = By.partialLinkText(value);
-			break;
-		default:
-			by = null;
-			break;
+	public static By locator(String locatorTpye, String value) 
+	{
+		By by=null;
+		try {
+			switch (locatorTpye) {
+			case "id":
+				by = By.id(value);
+				break;
+			case "name":
+				by = By.name(value);
+				break;
+			case "xpath":
+				by = By.xpath(value);
+				break;
+			case "css":
+				by = By.cssSelector(value);
+				break;
+			case "linkText":
+				by = By.linkText(value);
+				break;
+			case "partialLinkText":
+				by = By.partialLinkText(value);
+				break;
+			default:
+				by = null;
+				break;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Element Not Found");
 		}
 		return by;
 	}
@@ -172,27 +448,9 @@ public class Action_Keywords {
 		}
 	}
 	
-/*	public void EnterText(String locatorType,String locatorValfromExcel,String textValue)
-	{
-		try
-		{
-		By valueOfLocator=locatorValue(locatorType, locatorValfromExcel);
-		
-		System.out.println("Locator Value in Action:- "+ valueOfLocator);
-		System.out.println("Entered Text Value is:-"+textValue);
-		driver.findElement(valueOfLocator).sendKeys(textValue);
-		}
-		catch(Exception e)
-		{
-			System.out.println("Enter text not executed.");
-			e.printStackTrace();
-		}
-	}*/
 	
 	public void click(String locatorValue,String locatortype)
 	{
-		/*System.out.println("Locator Val for CLick:-" +locatorVal);
-		System.out.println("Locator Val for CLickData:-" +data);*/
 		driver.findElement(By.xpath(locatorValue)).click();
 	}
 	
